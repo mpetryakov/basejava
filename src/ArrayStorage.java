6626ea99da437fda22fd5f57df76db05aae06575
mpetryakov <bo0omerang71@gmail.com>
2018-12-19 19:45:38 +0300

    ArrayStorage implementation

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -2,29 +2,67 @@
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
+    final int length = 3;
+
+    Resume[] storage = new Resume[length];
+    int size = 0;
 
     void clear() {
+        for (int i=0; i<size; i++){
+            storage[i] = null;
+        }
+        size=0;
     }
 
     void save(Resume r) {
+        if(size < length) {
+            boolean uuidAlreadyExist = false;
+            for (int i = 0; i < size; i++) {
+                if (storage[i].uuid.equalsIgnoreCase(r.uuid)) {
+                    uuidAlreadyExist = true;
+                    System.err.println(String.format("Resume %s already exist in storage", r.uuid));
+                    break;
+                }
+            }
+            if(!uuidAlreadyExist) {
+                storage[size] = r;
+                size++;
+            }
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume r = null;
+        for (int i=0; i<size; i++){
+            if(storage[i].uuid.equalsIgnoreCase(uuid)){
+                r = storage[i];
+                break;
+            }
+        }
+        return r;
     }
 
     void delete(String uuid) {
+        for (int i=0; i<size; i++) {
+            if (storage[i].uuid.equalsIgnoreCase(uuid)) {
+                for (int j = i; j < size-1; j++) {
+                    storage[j] = storage[j + 1];
+                }
+                storage[size-1]=null;
+                size--;
+                break;
+            }
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        return storage;
     }
 
     int size() {
-        return 0;
+        return size;
     }
 }
